a3c2f3019029f6f996eb6322c2607022c4db3ae2
Dante Helmore <frostu8@protonmail.com>
2020-08-18 10:08:49 -0400

    added maths (good)

---
+++ src/math.rs
@@ -0,0 +1,9 @@
+pub use cgmath::prelude::*;
+
+pub type FLOAT = f64;
+
+pub type Vector2 = cgmath::Vector2<FLOAT>;
+pub type Vector3 = cgmath::Vector3<FLOAT>;
+
+pub type Matrix3 = cgmath::Vector3<FLOAT>;
+
