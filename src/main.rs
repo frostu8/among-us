036426e69dfff62593df023481a9efa9547d3e99
Dante Helmore <frostu8@protonmail.com>
2020-08-21 10:12:32 -0400

    added collision test to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use among_us::collide::{Circle, Polygon, Geometry as _};
+use among_us::math::*;
+
 fn main() {
-    println!("Hello, world!");
+    let polygon: Polygon = [Vector2::new(0.0, 2.0), Vector2::new(0.0, -2.0), Vector2::new(-4.0, -2.0), Vector2::new(-4.0, 2.0)].iter().map(|v| v + Vector2::new(10.0, 10.0)).collect::<Vec<Vector2>>().into();
+
+    if polygon.collide(&Circle::new(Vector2::new(15.0, 10.0), 5.000001)) {
+        println!("collision");
+    } else {
+        println!("no collision");
+    }
 }
