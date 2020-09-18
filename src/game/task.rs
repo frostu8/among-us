0db13bb859dc0f70d63a38bfa055f67e90e3184e
Dante Helmore <frostu8@protonmail.com>
2020-09-18 16:50:52 -0400

    shrunk tasks; don't want to get ahead of myself

--- src/game/task.rs
+++ src/game/task.rs
@@ -20,108 +20,16 @@
 use crate::game::State;
 
 /// A task.
+///
+/// This is purely the data part of a task.
 pub struct Task {
-    info: TaskInfo,
-    minigame: Box<dyn Minigame>,
+    name: String,
 }
 
 impl Task {
-    /// Create a new task.
-    ///
-    /// This uses the minigame's default implementation
-    pub fn new<T>(name: String) -> Task 
-    where T: Minigame + Default + 'static {
-        Task::new_with(name, T::default())
-    }
-
-    /// Create a new task from a [`Minigame`] type.
-    pub fn new_with<T>(name: String, minigame: T) -> Task
-    where T: Minigame + 'static {
-        Task {
-            info: TaskInfo {
-                name,
-                completion: 0.0,
-            },
-            minigame: Box::new(minigame),
-        }
-    }
-
     /// The name of the task.
     pub fn name(&self) -> &str {
-        &self.info.name
-    }
-
-    /// The completion rate of the task.
-    ///
-    /// If the completion rate is 1, then the task is considered complete.
-    pub fn completion(&self) -> f32 {
-        self.info.completion
-    }
-
-    /// Begin the task, initializing the minigame.
-    pub fn begin(&mut self, state: State) {
-        self.minigame.begin(state, &mut self.info);
-    }
-}
-
-/// Task information.
-pub struct TaskInfo {
-    pub name: String,
-    pub completion: f32,
-}
-
-/// A task pool.
-///
-/// This pool gaurentees that task IDs will stay consistent until the pool is
-/// cleared, i.e. at the end of a game.
-pub struct TaskPool {
-    pool: Vec<Option<Task>>,
-}
-
-impl TaskPool {
-    /// Index into the `TaskPool` immutably.
-    pub fn get(&self, id: usize) -> Option<&Task> {
-        self.pool.get(id)?.into()
-    }
-
-    /// Index into the `TaskPool` mutably.
-    pub fn get_mut(&mut self, id: usize) -> Option<&mut Task> {
-        self.pool.get_mut(id)?.into()
-    }
-
-    /// Insert a task with an id.
-    ///
-    /// Returns the old task that was in its place, or `None` if there was no
-    /// task in its place.
-    pub fn insert_with(&mut self, task: Task, id: usize) -> Option<Task> {
-        if id >= self.pool.len() {
-            // allocate memory to add the Task
-            self.pool.extend((0..=(self.pool.len() - id)).map(|_| None))
-        }
-
-        std::mem::replace(&mut self.pool[id], Some(task))
-    }
-
-    /// Insert at the first empty task id.
-    ///
-    /// Returns the id of the new task. 
-    pub fn insert(&mut self, task: Task) -> usize {
-        let id = self.first_empty();
-
-        self.insert_with(task, id);
-        id
-    }
-
-    /// Clears the pool.
-    pub fn clear(&mut self) {
-        self.pool.iter_mut().for_each(|t| *t = None);
-    }
-
-    fn first_empty(&self) -> usize {
-        match self.pool.iter().enumerate().find(|(_, t)| t.is_none()) {
-            Some((id, _)) => id,
-            None => self.pool.len(),
-        }
+        &self.name
     }
 }
 
